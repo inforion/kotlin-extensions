79a598bac526c54873ef1813b1c4c81ff1c2d956
Vladimir Davydov <v.davydov@inforion.ru>
2019-04-22 16:29:35 +0300

    Tests have been added

---
+++ src/test/kotlin/ru/inforion/lab403/common/extensions/BitopsKtTest.kt
@@ -0,0 +1,130 @@
+package ru.inforion.lab403.common.extensions
+
+import org.junit.Assert.*
+import org.junit.Test
+import kotlin.test.assertFails
+
+/**
+ * Created by davydov_vn on 22/04/19.
+ */
+
+class BitopsKtTest {
+    private val x = 0xDEADBEEF
+
+    @Test fun getTest_p0_p4() = assertEquals(0xF, x[3..0])
+    @Test fun getTest_p4_p8() = assertEquals(0xE, x[7..4])
+    @Test fun getTest_p8_p12() = assertEquals(0xE, x[11..8])
+    @Test fun getTest_p12_p16() = assertEquals(0xB, x[15..12])
+    @Test fun getTest_p16_p20() = assertEquals(0xD, x[19..16])
+    @Test fun getTest_p20_p24() = assertEquals(0xA, x[23..20])
+    @Test fun getTest_p24_p28() = assertEquals(0xE, x[27..24])
+    @Test fun getTest_p28_p32() = assertEquals(0xD, x[31..28])
+
+    @Test fun bitMask() {
+//        assertEquals(bitMask(0), 0x01)
+        assertFails { bitMask(0) }
+        assertEquals(bitMask(1), 0x01)
+        assertEquals(bitMask(2), 0x03)
+        assertEquals(bitMask(3), 0x07)
+        assertEquals(bitMask(4), 0x0F)
+        assertEquals(bitMask(5), 0x1F)
+        assertEquals(bitMask(6), 0x3F)
+        assertEquals(bitMask(7), 0x7F)
+
+        assertEquals(bitMask(11), 0x0000_07FF)
+        assertEquals(bitMask(15), 0x0000_7FFF)
+        assertEquals(bitMask(19), 0x0007_FFFF)
+        assertEquals(bitMask(23), 0x007F_FFFF)
+        assertEquals(bitMask(27), 0x07FF_FFFF)
+        assertEquals(bitMask(31), 0x7FFF_FFFF)
+        assertEquals(bitMask(32), 0xFFFF_FFFF)
+        assertEquals(bitMask(64), -1)
+        assertFails { bitMask(65) }
+
+//        assertEquals(bitMask(63), "FFFFFFFFFFFFFFFF".hexAsULong)
+    }
+
+    @Test fun bitMaskRange() {
+        assertEquals(bitMask(7..4), 0xF0)
+        assertEquals(bitMask(7..0), 0xFF)
+
+        assertEquals(bitMask(11..0), 0x0000_0FFF)
+        assertEquals(bitMask(15..0), 0x0000_FFFF)
+        assertEquals(bitMask(19..0), 0x000F_FFFF)
+        assertEquals(bitMask(23..0), 0x00FF_FFFF)
+        assertEquals(bitMask(27..0), 0x0FFF_FFFF)
+        assertEquals(bitMask(31..0), 0xFFFF_FFFF)
+
+        assertEquals(bitMask(31..16), 0xFFFF_0000)
+
+        assertEquals(bitMask(63..0), "FFFFFFFFFFFFFFFF".hexAsULong)
+    }
+
+    @Test fun maskRange() {
+        assertEquals(0xAD mask 7..4, 0xA0)
+        assertEquals(0xAD mask 7..0, 0xAD)
+        assertEquals(0xAD mask 6..5, 0x20)
+        assertEquals(0xAD mask 4..3, 0x08)
+        assertEquals(0xAD mask 5..2, 0x2C)
+        assertEquals(0xAD mask 15..0, 0xAD)
+        assertEquals(0xAD mask 63..14, 0x00)
+        assertEquals(0xAD mask 3..2, 0x0C)
+        assertEquals(0xAD mask 0..0, 0x01)
+    }
+
+    @Test fun signext() {
+        // TODO
+    }
+
+    @Test fun bext() {
+        // TODO
+    }
+
+    @Test fun insertBit() {
+        // TODO
+    }
+
+    @Test fun clearBit() {
+        assertEquals(0xFE, 0xFF clr 0)
+        assertEquals(0xFD, 0xFF clr 1)
+        assertEquals(0xFB, 0xFF clr 2)
+        assertEquals(0xF7, 0xFF clr 3)
+        assertEquals(0xEF, 0xFF clr 4)
+        assertEquals(0xDF, 0xFF clr 5)
+        assertEquals(0xBF, 0xFF clr 6)
+        assertEquals(0x7F, 0xFF clr 7)
+    }
+
+    @Test fun setBit() {
+        assertEquals(0x01, 0 set 0)
+        assertEquals(0x02, 0 set 1)
+        assertEquals(0x04, 0 set 2)
+        assertEquals(0x08, 0 set 3)
+        assertEquals(0x10, 0 set 4)
+        assertEquals(0x20, 0 set 5)
+        assertEquals(0x40, 0 set 6)
+        assertEquals(0x80, 0 set 7)
+    }
+
+    @Test fun insertField() {
+        assertEquals(insertField(0xF2, 0x0B, 3..0), 0xFB)
+        assertEquals(insertField(0xF2, 0, 3..0), 0xF0)
+    }
+
+    @Test fun isIntegerOverflow() {
+        // TODO
+    }
+
+    @Test fun swap32() {
+        assertEquals(0xAABBCCDD.swap32(), 0xDDCCBBAA)
+    }
+
+    @Test fun swap16() {
+        assertEquals(0xAABB.swap16(), 0xBBAA)
+    }
+
+    @Test fun cat() {
+        assertEquals(0xFAL, cat(0xFL, 0xAL, 3))
+        assertEquals(0xFA, cat(0xF, 0xA, 3))
+    }
+}
---
+++ src/test/kotlin/ru/inforion/lab403/common/extensions/RandomTest.kt
@@ -0,0 +1,71 @@
+package ru.inforion.lab403.common.extensions
+
+import org.junit.Test
+import org.junit.Assert.*
+
+/**
+ * Created by davydov_vn on 22/04/19.
+ */
+
+class RandomTest {
+
+    private val tolerance = 0.01
+    private val collectionSize = 100000
+
+    private data class Item(val id: Int, val weight: Double)
+    private val x = arrayListOf(Item(3, 0.1), Item(4, 0.45), Item(0, 0.15), Item(1, 0.25), Item(2, 0.05))
+    private val q = Array(collectionSize, { x.choice { it.weight }})
+    private val pq = x.map { item -> q.count { it.id == item.id }.toDouble() / q.count() }
+
+    private val z = Array(collectionSize, { x.choice() })
+    private val pz = x.map { item -> z.count { it.id == item.id }.toDouble() / z.count() }
+
+    private val y = Array(collectionSize, { x.choice { 0.0 } })
+    private val py = x.map { item -> y.count { it.id == item.id }.toDouble() / z.count() }
+
+    @Test fun choiceBasicTest() {
+        assertEquals(x.sumByDouble { it.weight }, 1.0, tolerance)
+    }
+
+    @Test fun choiceAccumTest() {
+        assertEquals(pq.sum(), 1.0, tolerance)
+    }
+
+    @Test fun choiceWeightedEachTest() {
+        x.zip(pq).forEach { assertEquals(it.first.weight, it.second, tolerance) }
+    }
+
+    @Test fun choiceUniform1EachTest() {
+        val w = 1.0 / x.size
+        x.zip(pz).forEach { assertEquals(w, it.second, tolerance) }
+    }
+
+    @Test fun choiceUniform0EachTest() {
+        val w = 1.0 / x.size
+        x.zip(py).forEach { assertEquals(w, it.second, tolerance) }
+    }
+
+    fun error(actual: Int, expected: Int): Double = (actual - expected) / expected.toDouble()
+
+    fun assertHistogram(data: ByteArray, size: Int) {
+        val histogram = math.histogram(data)
+        val bad = histogram.find { error(it.count, size) > 0.01 }
+        assert(bad == null) { "Found bad bin: $bad -> error = ${error(bad!!.count, size)}" }
+    }
+
+    @Test
+    fun randbytes() {
+        assertHistogram(random.randbytes(100000), 100000)
+    }
+
+    @Test
+    fun success() {
+        val prob = 0.69
+        val total = 1_000_000
+        val data = Array(total) { random.success(prob).toInt() }
+        val result = data.count { it == 1 } / total.toDouble()
+        val error = Math.abs(result - prob)
+        println("Positive success = $result, error = $error")
+        assert(error < 0.01)
+    }
+}
