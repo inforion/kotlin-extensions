b6333f3fd3a8ad470edc515a224ea2d322a170c9
Alexei Gladkikh <a.gladkikh@inforion.ru>
2020-08-20 10:31:19 +0300

    Moved strings from proposal

--- src/main/kotlin/ru/inforion/lab403/common/extensions/strings.kt
+++ src/main/kotlin/ru/inforion/lab403/common/extensions/strings.kt
@@ -2,6 +2,7 @@
 
 package ru.inforion.lab403.common.extensions
 
+import java.io.File
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 import java.nio.charset.Charset
@@ -23,7 +24,7 @@ fun String.toCamelCase(): String {
     return this
 }
 
-fun ByteArray.ascii(): String = map { if (it >= 0x20 && it < 0x7F) it.toChar() else '.' }.joinToString("")
+fun ByteArray.ascii(): String = map { if (it in 0x20..0x7e) it.toChar() else '.' }.joinToString("")
 
 fun ByteArray.hexlify(upperCase: Boolean = true, separator: Char? = null): String {
     val hexChars = CharArray(this.size * 2)
@@ -237,3 +238,193 @@ inline fun String.stretch(maxlen: Int, alignRight: Boolean = true): String {
 }
 
 val emptyString: String = String()
+
+/**
+ * {EN}
+ * Makes [File] from a [String]
+ *
+ * @return [File]
+ * {EN}
+ */
+fun String.toFile() = File(this)
+
+/**
+ * {EN}
+ * Makes [File] from [String] and append [child] to it.
+ * Also checks if parent is blank avoid absolute path creation
+ *
+ * @param child Path suffix
+ * @return [File]
+ * {EN}
+ */
+fun String.toFile(child: String): File {
+    // If parent is blank then absolute path will be created
+    // "".toFile("temp") -> /temp
+    if (isBlank()) return File(child)
+    return File(this, child)
+}
+
+/**
+ * {EN}
+ * Checks whether current [String] is path to a some file on filesystem. Wrapper to [File.isFile].
+ *
+ * @return true if and only if the file denoted by this abstract pathname exists and is a normal file
+ * {EN}
+ */
+fun String.isFile() = toFile().isFile
+
+/**
+ * {EN}
+ * Checks whether current [String] is path to a some directory on filesystem. Wrapper to [File.isDirectory].
+ *
+ * @return true if and only if the file denoted by this abstract pathname exists and is a directory
+ * {EN}
+ */
+fun String.isDirectory() = toFile().isDirectory
+
+/**
+ * {EN}
+ * Checks whether current [String] is absolute path. Wrapper to [File.isAbsolute].
+ *
+ * @return true if this abstract pathname is absolute
+ * {EN}
+ */
+fun String.isAbsolute() = toFile().isAbsolute
+
+/**
+ * {EN}
+ * Creates the directory named by this abstract pathname, including any
+ * necessary but nonexistent parent directories.  Note that if this
+ * operation fails it may have succeeded in creating some of the necessary
+ * parent directories.
+ *
+ * Wrapper to [File.mkdirs].
+ *
+ * @return true if and only if the directory was created, along with all necessary parent directories
+ * {EN}
+ */
+fun String.mkdirs() = toFile().mkdirs()
+
+/**
+ * {EN}
+ * Returns an array of abstract pathnames denoting the files in the
+ * directory denoted by this abstract pathname.
+ *
+ * Wrapper to [File.listFiles].
+ *
+ * @return  An array of abstract pathnames denoting the files and
+ *          directories in the directory denoted by this abstract pathname.
+ *          The array will be empty if the directory is empty.  Throw [IllegalArgumentException]
+ *          if this abstract pathname does not denote a directory, or if an I/O error occurs.
+ * {EN}
+ */
+fun String.listdir(): Array<File> = toFile().listFiles() ?: throw IllegalArgumentException("Can't list directory $this!")
+
+/**
+ * {EN}
+ * Returns an array of abstract pathnames denoting the files and
+ * directories in the directory denoted by this abstract pathname that
+ * satisfy the specified filter.
+ *
+ * Wrapper to [File.listFiles].
+ *
+ * @param predicate A file filter
+ *
+ * @return  An array of abstract pathnames denoting the files and
+ *          directories in the directory denoted by this abstract pathname.
+ *          The array will be empty if the directory is empty.  Throw [IllegalArgumentException]
+ *          if this abstract pathname does not denote a directory, or if an I/O error occurs.
+ * {EN}
+ */
+fun String.listdir(predicate: (File) -> Boolean): Array<File> = toFile().listFiles(predicate)
+    ?: throw IllegalArgumentException("Can't list directory $this!")
+
+/**
+ * {EN}
+ * Checks whether [String] has ends with specified [extension] and if no then add [extension] to [String]
+ *
+ * @param extension A suffix to append to [String]
+ *
+ * @return A string with suffix without duplicate it
+ * {EN}
+ */
+fun String.addExtension(extension: String): String = if (!endsWith(extension)) this + extension else this
+
+
+/**
+ * {EN}
+ * Operator function make possible to join paths using / operation between [File] and [String]
+ * val base = File("base")
+ * val fullpath = base / "test"
+ *
+ * @param child path suffix
+ *
+ * @return joined paths
+ * {EN}
+ */
+operator fun String.div(child: String): String = toFile(child).path
+
+/**
+ * {EN}
+ * Repeats [String] [n] times.
+ *
+ * @param n A number to repeat the string
+ *
+ * @return repeated [n] times string
+ * {EN}
+ */
+operator fun String.times(n: Int) = repeat(n)
+
+/**
+ * Splits this char sequence around matches of the given regular expression and remove leading and trailing whitespace.
+ *
+ * @param regex A regex expression
+ *
+ * @return A list of trimmed strings split by [regex] expression
+ */
+fun String.splitTrim(regex: Regex) = split(regex).map { it.trim() }
+
+/**
+ * Splits this char sequence to a list of strings around occurrences of the
+ * specified [delimiters] and remove leading and trailing whitespace.
+ *
+ * @param delimiters One or more strings to be used as delimiters.
+ * @param ignoreCase `true` to ignore character case when matching a delimiter. By default `false`.
+ *
+ * @return A list of trimmed strings split by [delimiters]
+ */
+fun String.splitTrim(vararg delimiters: String, ignoreCase: Boolean = false) =
+    split(*delimiters, ignoreCase = ignoreCase).map { it.trim() }
+
+/**
+ * Splits this char sequence to a list of strings by any whitespaces
+ *
+ * @return A list of strings split by any whitespace characters
+ */
+fun String.splitWhitespaces() = splitTrim(Regex("\\s+"))
+
+/**
+ * Returns a substring after the last occurrence of [start] and before the first occurrence of [end]
+ * If the string does not contain the [start] or [end] throw an error.
+ *
+ * @param start delimiter after which start new string
+ * @param end delimiter before which end new string
+ *
+ * @return A substring between [start] and [end]
+ */
+fun String.substringBetween(start: String, end: String): String {
+    val startIndex = indexOf(start)
+    require(startIndex >= 0)
+    val endIndex = indexOf(end)
+    require(endIndex >= 0)
+    return substring(startIndex + 1, endIndex)
+}
+
+
+inline fun String.classpathToPath() = replace(".", "/")
+
+inline fun String.className() = substringAfterLast(".")
+
+inline fun String.packageName() = substringBeforeLast(".")
+
+inline fun String.splitPackageClass() = Pair(packageName(), className())
