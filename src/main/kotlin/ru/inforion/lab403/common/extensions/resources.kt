f87e545af89fc83a13b864d9a3c6eab54719e02a
Alexei Gladkikh <a.gladkikh@inforion.ru>
2020-09-01 10:58:32 +0300

    Added resources loading from root folder

--- src/main/kotlin/ru/inforion/lab403/common/extensions/resources.kt
+++ src/main/kotlin/ru/inforion/lab403/common/extensions/resources.kt
@@ -2,27 +2,50 @@ package ru.inforion.lab403.common.extensions
 
 import java.io.FileNotFoundException
 import java.io.InputStream
+import java.net.URL
 import java.util.*
 import java.util.zip.GZIPInputStream
 
-/**
- * Created by Alexei Gladkikh on 29/01/17.
- */
-inline fun <reified T: Any>getResourceAsStreamOf(obj: T, resource: String): InputStream {
+inline fun <reified T: Any, R: Any> getResourceOf(obj: T, resource: String, getter: (Class<T>) -> R?): R {
     val cls: Class<T> = obj.javaClass
-    val stream = cls.getResourceAsStream(resource)
-    if (stream == null) {
+    val result = getter(cls)
+    if (result == null) {
         val basepath = cls.getResource("")
-        throw FileNotFoundException("Can't open resource $resource with path $basepath class $cls")
+        throw FileNotFoundException("Can't open resource $resource with path $basepath of $cls")
     }
-    return stream
+    return result
 }
 
-inline fun <reified T: Any>getGZIPResourceAsStreamOf(obj: T, resource: String): InputStream =
+inline fun <reified T: Any> getResourceAsStreamOf(obj: T, resource: String): InputStream =
+    getResourceOf(obj, resource) { it.getResourceAsStream(resource) }
+
+inline fun <reified T: Any>getGZIPResourceAsStreamOf(obj: T, resource: String) =
         GZIPInputStream(getResourceAsStreamOf(obj, resource))
 
-inline fun <reified T: Any> T.getResourceAsStream(resource: String): InputStream = getResourceAsStreamOf(this, resource)
-inline fun <reified T: Any> T.getGZIPResourceAsStream(resource: String): InputStream = getGZIPResourceAsStreamOf(this, resource)
+inline fun <reified T: Any> T.getResourceAsStream(resource: String) = getResourceAsStreamOf(this, resource)
+
+inline fun <reified T: Any> T.getGZIPResourceAsStream(resource: String) = getGZIPResourceAsStreamOf(this, resource)
+
+inline fun <reified T: Any>getResourceUrlOf(obj: T, resource: String): URL =
+    getResourceOf(obj, resource) { it.getResource(resource) }
+
+inline fun <reified T: Any> T.getResourceUrl(resource: String): URL = getResourceUrlOf(this, resource)
+
+private fun getClassLoader() = Thread.currentThread().contextClassLoader
+
+fun <R: Any> getResourceOfRoot(resource: String, getter: (ClassLoader) -> R?): R {
+    val cl = getClassLoader()
+    val result = getter(cl)
+    if (result == null) {
+        val basepath = cl.getResource("")
+        throw FileNotFoundException("Can't open resource $resource with path $basepath")
+    }
+    return result
+}
+
+fun getResourceAsStreamOfRoot(resource: String): InputStream = getResourceOfRoot(resource) { it.getResourceAsStream(resource) }
+
+fun getResourceUrlOfRoot(resource: String): URL = getResourceOfRoot(resource) { it.getResource(resource) }
 
 inline fun <reified T: Any> T.buildInformationString(versionProperties: String = "version.properties"): String {
     val result: String
