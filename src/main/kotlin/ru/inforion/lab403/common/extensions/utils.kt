475522d8ae9f9401cc98709da23378c9fa11951d
Alexei Gladkikh <a.gladkikh@inforion.ru>
2020-11-15 11:51:57 +0300

    Added sure function

---
+++ src/main/kotlin/ru/inforion/lab403/common/extensions/utils.kt
@@ -0,0 +1,10 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
+package ru.inforion.lab403.common.extensions
+
+/**
+ * Function works like Kotlin org.jetbrains.kotlin.utils.sure from kotlin-compiler-embeddable
+ *
+ * If [this] is not null then returns [this] else throws exception with specified message
+ */
+inline fun <T: Any> T?.sure(message: () -> String) = this ?: throw IllegalArgumentException(message())
