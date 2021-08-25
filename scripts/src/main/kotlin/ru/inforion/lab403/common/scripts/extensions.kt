ae7d06a817dbe1a469d16b21e0421d0e52ba5b5b
Simankov Artem <a.simankov@inforion.ru>
2021-08-25 17:27:09 +0300

    Added getters for PyException fields

--- scripts/src/main/kotlin/ru/inforion/lab403/common/scripts/extensions.kt
+++ scripts/src/main/kotlin/ru/inforion/lab403/common/scripts/extensions.kt
@@ -1,8 +1,11 @@
 package ru.inforion.lab403.common.scripts
 
-import org.python.core.PyClass
-import org.python.core.PyException
+import org.python.core.*
 
 val PyException.pyClass get(): PyClass = type as PyClass
 
 val PyException.pyClassOrNull get(): PyClass? = type as? PyClass
+
+val PyException.pyType get(): PyType = type as PyType
+
+val PyException.pyValue get(): PyBaseExceptionDerived = value as PyBaseExceptionDerived
