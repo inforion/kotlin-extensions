84d82a574120a7602e0a11a67730a6e258c2145b
Simankov Artem <a.simankov@inforion.ru>
2021-08-26 19:11:00 +0300

    Added more getters for different scenario

--- scripts/src/main/kotlin/ru/inforion/lab403/common/scripts/extensions.kt
+++ scripts/src/main/kotlin/ru/inforion/lab403/common/scripts/extensions.kt
@@ -8,4 +8,19 @@ val PyException.pyClassOrNull get(): PyClass? = type as? PyClass
 
 val PyException.pyType get(): PyType = type as PyType
 
-val PyException.pyValue get(): PyBaseExceptionDerived = value as PyBaseExceptionDerived
+val PyException.pyJavaType get(): PyJavaType = type as PyJavaType
+
+val PyException.pyObjectDerivedValue get(): PyObjectDerived = value as PyObjectDerived
+
+val PyException.pyBaseExceptionDerivedValue get(): PyBaseExceptionDerived = value as PyBaseExceptionDerived
+
+val PyException.pyValueString get(): PyString = value as PyString
+
+val PyObjectDerived.message get(): String = __findattr__("message").toString()
+
+val PyType.pyBase get(): PyJavaType = base as PyJavaType
+
+val PyJavaType.pyBase get(): PyJavaType = base as PyJavaType
+
+// Python Exceptions that inherited from Java Exception encapsulated in proxy PyTypes
+val PyException.proxyType get(): Class<*> = pyType.pyBase.pyBase.proxyType
