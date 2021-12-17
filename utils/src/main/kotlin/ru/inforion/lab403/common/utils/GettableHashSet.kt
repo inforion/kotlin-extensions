e25353b6691cf9dafac00e65687905ede7b11ad8
Alexei Gladkikh <a.gladkikh@inforion.ru>
2021-12-17 14:49:00 +0300

    Added GettableHashSet

---
+++ utils/src/main/kotlin/ru/inforion/lab403/common/utils/GettableHashSet.kt
@@ -0,0 +1,43 @@
+package ru.inforion.lab403.common.utils
+
+import ru.inforion.lab403.common.extensions.cast
+import ru.inforion.lab403.common.extensions.int
+import java.io.Serializable
+import java.util.AbstractSet
+
+class GettableHashSet<E> private constructor(
+    private val map: HashMap<Any, E> = HashMap()
+) : AbstractSet<E>(), MutableSet<E>, Serializable {
+
+    companion object {
+        private fun <E> map(size: Int): HashMap<Any, E> {
+            val supposedCapacity = (size / .75f).int + 1
+            val initialCapacity = supposedCapacity.coerceAtLeast(16)
+            return HashMap(initialCapacity)
+        }
+    }
+
+    constructor() : this(HashMap())
+
+    constructor(c: Collection<E>) : this(map(c.size)) { addAll(c) }
+
+    constructor(initialCapacity: Int, loadFactor: Float) : this(HashMap(initialCapacity, loadFactor))
+
+    constructor(initialCapacity: Int) : this(HashMap(initialCapacity))
+
+    override fun iterator(): MutableIterator<E> = map.keys.iterator().cast()
+
+    override fun isEmpty() = map.isEmpty()
+
+    override fun contains(element: E) = map.containsKey(element.cast())
+
+    override fun add(element: E) = map.put(element.cast(), element) == null
+
+    override fun remove(element: E): Boolean = map.remove(element.cast()) != null
+
+    operator fun get(element: Any): E? = map[element]
+
+    override val size get(): Int = map.size
+
+    override fun clear(): Unit = map.clear()
+}
--- utils/src/main/kotlin/ru/inforion/lab403/common/utils/functions.kt
+++ utils/src/main/kotlin/ru/inforion/lab403/common/utils/functions.kt
@@ -3,3 +3,8 @@
 package ru.inforion.lab403.common.utils
 
 inline fun <R, T>lazyTransient(noinline initializer: () -> T) = LazyTransient<R, T>(initializer)
+
+inline fun <E> gettableHashSetOf(vararg values: E) = GettableHashSet<E>().apply { addAll(values) }
+
+inline fun <E> GettableHashSet<E>.getValue(key: Any): E = get(key)
+    ?: throw NoSuchElementException("Key $key is missing in the set.")
