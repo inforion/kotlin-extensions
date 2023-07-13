4e28bb728772c0f51333805cbef655af41646394
Anatolii Titov <a.titov@inforion.ru>
2023-07-13 19:56:11 +0300

    Add basic slf4j marker factory and mdc adapter

--- logging/src/main/kotlin/ru/inforion/lab403/common/logging/logger/LoggerProvider.kt
+++ logging/src/main/kotlin/ru/inforion/lab403/common/logging/logger/LoggerProvider.kt
@@ -2,22 +2,18 @@ package ru.inforion.lab403.common.logging.logger
 
 import org.slf4j.ILoggerFactory
 import org.slf4j.IMarkerFactory
+import org.slf4j.helpers.BasicMDCAdapter
 import org.slf4j.spi.MDCAdapter
 import org.slf4j.spi.SLF4JServiceProvider
+import org.slf4j.helpers.BasicMarkerFactory
 
-class LoggerProvider: SLF4JServiceProvider {
-
+class LoggerProvider : SLF4JServiceProvider {
     override fun getLoggerFactory(): ILoggerFactory {
         return LoggerFactory()
     }
 
-    override fun getMarkerFactory(): IMarkerFactory {
-        throw NotImplementedError("Marker logging not implemented")
-    }
-
-    override fun getMDCAdapter(): MDCAdapter {
-        throw NotImplementedError("MDCA adapter not implemented")
-    }
+    override fun getMarkerFactory(): IMarkerFactory = BasicMarkerFactory()
+    override fun getMDCAdapter(): MDCAdapter = BasicMDCAdapter()
 
     override fun getRequestedApiVersion(): String {
         return VERSION
